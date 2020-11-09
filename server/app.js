b17009b0c3ce49b156184f71734317d813378986
purgatorygates <ulisesrivero1984@gmail.com>
2020-11-09 15:40:50 -0500

    second half of 11_9 lecture

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
-require('./db/config/index')
-const express = require('express'),
+require("./db/config/index");
+const express = require("express"),
   app = express();
 
 // Parse incoming JSON into objects
 app.use(express.json());
+
+// Unauthenticated Routes
+app.use("/api/users", openRoutes);
 
 module.exports = app;
